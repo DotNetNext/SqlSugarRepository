4173aa2ccf8a925a4bb0e0fe58710645224692dc
sunkaixuan <610262374@qq.com>
2016-11-19 14:17:17 +0800

    -

--- OrmTest/SqlSugarRepository/Sugar/MySql/Append/MySqlSugarClient.cs
+++ OrmTest/SqlSugarRepository/Sugar/MySql/Append/MySqlSugarClient.cs
@@ -63,12 +63,12 @@ namespace SqlSugarRepository
 
         public void AddDisableUpdateColumns(params string[] columns)
         {
-            _db.AddDisableUpdateColumn(columns);
+            _db.AddDisableUpdateColumns(columns);
         }
 
-        public void AddMappingColumn(KeyValue mappingColumns)
+        public void AddMappingColumn(KeyValue mappingColumn)
         {
-            _db.AddMappingColum(new MySqlSugar.KeyValue() { Key = mappingColumns.Key, Value = mappingColumns.Value });
+            _db.AddMappingColumn(new MySqlSugar.KeyValue() { Key = mappingColumn.Key, Value = mappingColumn.Value });
         }
 
         public void AddMappingTable(KeyValue mappingTable)
