6693a3e3ff042f7419a318ce41ec6230e9c520bc
sunkaixuan <610262374@qq.com>
2016-11-19 20:57:56 +0800

    -

---
+++ OrmTest/OrmTest/Demos/InsertOrUpdate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NewTest.Dao;
+using Models;
+using System.Data.SqlClient;
+using SqlSugar;
+using SqlSugarRepository;
+
+namespace NewTest.Demos
+{
+    //插入
+    public class InsertOrUpdate 
+    {
+
+        public void Init(DbType type)
+        {
+            Console.WriteLine("启动Inset.Init");
+            using (var db = SugarDao.GetInstance(type))
+            {
+
+                db.InsertOrUpdate(GetInsertItem1());//执行插入
+
+                db.InsertOrUpdate(GetInsertItem2());//执行更新
+            }
+        }
+
+        private static Student GetInsertItem1()
+        {
+            Student s = new Student()
+            {
+                name = "张" + new Random().Next(1, int.MaxValue)
+            };
+            return s;
+        }
+        private static Student GetInsertItem2()
+        {
+            Student s = new Student()
+            {
+                id=14,
+                name = "张" + new Random().Next(1, int.MaxValue)
+            };
+            return s;
+        }
+    }
+}
--- OrmTest/OrmTest/Program.cs
+++ OrmTest/OrmTest/Program.cs
@@ -16,12 +16,16 @@ namespace OrmTest
  
 
             //设置执行的DEMO
-            string switchOn = "select";
+            string switchOn = "insert";
+            //数据库类型
+            DbType type = DbType.SqlServer;
             switch (switchOn)
             {
                 /****************************基本功能**************************************/
                 //查询
-                case "select": new Select().Init(DbType.Oracle); break;
+                case "select": new Select().Init(type); break;
+                case "insert": new Insert().Init(type); break;
+                case "insertorupdate": new InsertOrUpdate().Init(type); break;
             }
             Console.WriteLine("执行成功请关闭窗口");
             Console.ReadKey();
